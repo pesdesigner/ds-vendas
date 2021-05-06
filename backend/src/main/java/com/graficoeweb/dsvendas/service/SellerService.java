7cca62ca70a7b634391fd9b9651597568a29bd0f
pesdesigner <pauloeduardo.dg@outlook.com>
2021-05-05 23:08:51 -0300

    Layers

---
+++ backend/src/main/java/com/graficoeweb/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.graficoeweb.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.graficoeweb.dsvendas.dto.SellerDTO;
+import com.graficoeweb.dsvendas.entities.Seller;
+import com.graficoeweb.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
