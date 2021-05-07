a0530c3955b887e818c312e84571a16339103c6f
pesdesigner <pauloeduardo.dg@outlook.com>
2021-05-06 23:26:21 -0300

    Group by search

--- backend/src/main/java/com/graficoeweb/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/graficoeweb/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.graficoeweb.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.graficoeweb.dsvendas.dto.SaleDTO;
+import com.graficoeweb.dsvendas.dto.SaleSuccessDTO;
+import com.graficoeweb.dsvendas.dto.SaleSumDTO;
 import com.graficoeweb.dsvendas.entities.Sale;
 import com.graficoeweb.dsvendas.repositories.SaleRepository;
 import com.graficoeweb.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepositoty.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
